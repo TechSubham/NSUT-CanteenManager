319179689f93a64ab25b678444f978f9e8c6239f
TechSubham <subhamchauhan523@gmail.com>
2025-01-18 04:07:23 +0530

    Notifications added

--- backend/server/firebaseAdmin.js
+++ backend/server/firebaseAdmin.js
@@ -2,17 +2,11 @@ const admin = require('firebase-admin');
 require('dotenv').config();
 
 const serviceAccount = {
-    type: "service_account",
+    type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-    universe_domain: "googleapis.com"
+    client_id: process.env.FIREBASE_CLIENT_ID
 };
 
 admin.initializeApp({
